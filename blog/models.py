09a4c6678a9030c4de1a928da46232a708fa610b
ddeodeok <ejrgus0000@naver.com>
2022-08-10 02:28:12 +0900

    345 8월9일

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,23 @@
 from venv import create
 from django.db import models
+from django.contrib.auth.models import User
 import os
+from distutils.command.upload import upload
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+# 수정하기
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     # hook_text = models.CharField(max_length=100, blank=True)
@@ -14,8 +29,12 @@ class Post(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f'[{self.pk}]{self.title}'
+    # author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    # category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+    # def __str__(self):
+    #     return f'[{self.pk}]{self.title} :: {self.author}'
 
 
     def get_absolute_url(self):
--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 #    path('<int:pk>/', views.single_post_page),
 #    path('', views.index),
   
+    path('category/<str:slug>/', views.category_page),
     path('',views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
 ]
