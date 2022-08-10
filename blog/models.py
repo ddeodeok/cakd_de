6477f6f204626acd3d5c5978247c83ee9846861c
ddeokdeok <ejrgus0000@naver.com>
2022-08-10 17:20:21 +0900

    220810

--- blog/models.py
+++ blog/models.py
@@ -20,21 +20,19 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    # hook_text = models.CharField(max_length=100, blank=True)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
-    # file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-    # author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-
-    # category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
-
-    # def __str__(self):
-    #     return f'[{self.pk}]{self.title} :: {self.author}'
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    def __str__(self):
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
 
     def get_absolute_url(self):
