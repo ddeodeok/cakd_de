82eb69b448e71b20dd9de2c8a2a111b0c4763328
ddeodeok <ejrgus0000@naver.com>
2022-08-12 15:20:41 +0900

    로그인기능 완성

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import sys
+import os
+# 부모 폴더의 절대경로를 참조 path에 추가하기
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from blog.models import Post, Category, Tag
+
 
 def landing(request):
     return render(
@@ -9,6 +15,12 @@ def landing(request):
 def about_me(request):
     return render(
         request,
-        'single_pages/about_me.html'
+        'single_pages/about_me.html',
+        {
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+        }
+        
     )
 # Create your views here.
+
