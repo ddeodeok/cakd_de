c966437cc63a9cd275390d665ecb8626a3cdac9e
ddeodeok <ejrgus0000@naver.com>
2022-08-18 15:52:43 +0900

    최신글(대문페이지)

--- single_pages/views.py
+++ single_pages/views.py
@@ -4,12 +4,19 @@ import os
 # 부모 폴더의 절대경로를 참조 path에 추가하기
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from blog.models import Post, Category, Tag
+from blog.models import Post
 
 
 def landing(request):
+    recent_posts = Post.objects.order_by('-pk')[:3]
     return render(
         request,
-        'single_pages/landing.html'
+        'single_pages/landing.html',
+        {
+            'recent_posts': recent_posts,
+            # 'categories': Category.objects.all(),
+            # 'no_category_post_count': Post.objects.filter(category=None).count(),
+        }
     )
 
 def about_me(request):
