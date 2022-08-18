8f097beb34069b8e122f34c8736875325e82660d
ddeodeok <ejrgus0000@naver.com>
2022-08-18 23:35:29 +0900

    program

--- program/views.py
+++ program/views.py
@@ -5,14 +5,14 @@ def inputdata(request):
     return render(request, 'program/inputdata.html')
 
 def result(request):
-    # lis=[]
-    # lis.append(request.GET['a'])
-    # lis.append(request.GET['b'])
+    lis=[]
+    lis.append(request.GET['a'])
+    lis.append(request.GET['b'])
 
-    # sum = 0
-    # for l in lis:
-    #     sum += int(l)
-    # ans = sum
+    sum = 0
+    for l in lis:
+        sum += int(l)
+    ans = sum
 
     return render(request, 'program/result.html',{'ans':ans,'lis':lis})
 
